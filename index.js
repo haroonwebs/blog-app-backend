3e2a7ca24c773635705b9649ae466eb6dcfe4784
Muhammad Haroon Talib <haroontalib8029@gmail.com>
2024-10-09 21:42:30 +0500

    added some changes

--- index.js
+++ index.js
@@ -20,12 +20,7 @@ dbconnection();
 
 // Middleware
 // app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
